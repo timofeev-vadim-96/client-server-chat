90792f1f0c85843c30714e6105ebc8bc5bc71fb4
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2024-02-18 15:56:15 +0300

    chat-client main edited

--- chat-client/src/main/java/web/app/client/Main.java
+++ chat-client/src/main/java/web/app/client/Main.java
@@ -8,6 +8,5 @@ import java.net.InetAddress;
 public class Main {
     public static void main(String[] args) throws IOException {
         ClientUI view = new ClientUI();
-        System.out.println(InetAddress.getLocalHost().getHostAddress());
     }
 }
